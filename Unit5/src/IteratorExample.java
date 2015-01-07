fcb7a934450d54401c4bb1cacb29536135c68888
dyla1920 <dyla1920@ISS147D8-14.scdsb.pri>
2015-01-07 09:18:00 -0500

    Unit 5

--- Unit5/src/IteratorExample.java
+++ Unit5/src/IteratorExample.java
@@ -7,12 +7,47 @@ public class IteratorExample {
     
     static ArrayList list = new ArrayList();
     static ListIterator li;
+    static int pos;
   
     public static void main(String[] args) {
         list.add("a");
         list.add("b");
         list.add("c");
         li = list.listIterator();
+        System.out.println("1-Move over item: "+li.next());
+        //move2 
+        System.out.println("Move over item"+ li.next());
+        pos++;
+        System.out.println("Remove" );
+        li.remove();
+        pos--;
+        //add b back
+        li.add("b");
+        pos++;
+        li.next();
+        pos++;
+        li.previous();
+        pos--;
+        //move7
+        System.out.println("Remove last item seen");
+        li.remove();
+        //move 8 - move back over item
+        li.previous();
+        pos--;
+        //move 9 - set last thing to e
+        li.set("e");
+        //move 10 - add b back in
+        li.add("b");
+        pos++;
+        //move 11 - add c
+        li.add("c");
+        pos++;
+        showList();
+    }
+    public static void showList(){
+        for (int x=0; x<list.size();x++){
+        //if(x==pos)
+    }
     }
     
 }
